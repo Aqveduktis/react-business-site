ca43ab99a7f3d76064c69b3b8b0a2d7d5c93f6b0
Sara <sara@Saras-MacBook-Pro.local>
2020-03-20 16:10:32 +0100

    Adding a header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Header} from './components/Header'
 
 export const App = () => {
   return (
     <div>
       Find me in src/app.js!
+      <Header title = "Coffie Shop" words = "best coffie in town" />
     </div>
   )
 }
